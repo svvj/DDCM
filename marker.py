731b83d6d387b53f73f39d29e6bae069e32c7ca0
svvj <wjdtmd0105@gmail.com>
2022-01-26 11:25:02 +0900

    Complete marker detecting

--- marker.py
+++ marker.py
@@ -1,30 +1,32 @@
-def findSubtreesInBFS(nodes, edges):
+def mean_int(list):
+    return int(sum(list) / len(list))
+
+def findSubgraphsInBFS(nodes, edges):
     i = 0
-    subtrees = []
+    subgraphs = []
 
     for node in nodes:
         linked_nodes = []
-        subtree = {'n': [], 'e': []}
+        subgraph = {'n': [], 'e': [], 'c': ()}
 
         for edge in edges:
             if node in edge:
                 node_index = edge.index(node)
 
-                if node not in subtree['n']:
-                    subtree['n'].append(node)
-                if edge not in subtree['e']:
-                    subtree['e'].append(edge)
+                if node not in subgraph['n']:
+                    subgraph['n'].append(node)
+                if edge not in subgraph['e']:
+                    subgraph['e'].append(edge)
 
-                if edge[1-node_index] not in subtree['n']:
-                    subtree['n'].append(edge[1-node_index])
+                if edge[1-node_index] not in subgraph['n']:
+                    subgraph['n'].append(edge[1-node_index])
                     linked_nodes.append(edge[1-node_index])
 
-        # findSubtreesInBFS(linked_nodes, edges)
+        if len(subgraph['n']) != 0:
+            subgraph['c'] = list(map(mean_int, zip(*subgraph['n'])))
+            subgraphs.append(subgraph)
 
-        if len(subtree['n']) != 0:
-            subtrees.append(subtree)
-
-    return subtrees
+    return subgraphs
 
 
 if __name__ == "__main__":
@@ -32,12 +34,12 @@ if __name__ == "__main__":
 
     e_list = [[(580, 711), (581, 711)], [(598, 665), (593, 655)], [(598, 665), (591, 654)], [(598, 665), (593, 652)], [(593, 655), (591, 654)], [(593, 655), (593, 652)], [(591, 654), (593, 652)], [(479, 643), (487, 640)], [(575, 623), (577, 622)], [(575, 623), (584, 621)], [(577, 622), (584, 621)]]
     n_list = [(487, 640), (581, 711), (591, 654), (433, 652), (575, 623), (479, 643), (593, 655), (593, 652), (498, 710), (595, 713), (577, 622), (598, 665), (584, 621), (456, 652), (580, 711)]
-    subtrees = findSubtreesInBFS(n_list, e_list)
+    subgraphs = findSubgraphsInBFS(n_list, e_list)
 
     x = []
     y = []
-    for subtree in subtrees:
-        e = subtree['e']
+    for subgraph in subgraphs:
+        e = subgraph['e']
         for edge in e:
             x.append(edge[0][0])
             x.append(edge[1][0])
--- video_marker.py
+++ video_marker.py
@@ -9,7 +9,7 @@ cap = cv.VideoCapture('240fps.mp4')
 # if not cap.isOpened():
 #     print("Cannot open camera")
 #     exit()
-section_num = 6
+section_num = 1
 width = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
 height = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
 width_section = width / section_num
@@ -20,8 +20,26 @@ print(f"width: {width}, height: {height}, fps: {fps}")
 fourcc = cv.VideoWriter_fourcc(*'mp4v')
 out = cv.VideoWriter('video_marker_240.mp4', fourcc, fps, (int(width), int(height)))
 
-save_video = False
+save_video = True
 visual_video = True
+
+
+def visualize_marker(markers):
+    dot_size = 4
+    circle_type = cv.FILLED
+    for node in markers['1']:
+        white = (255, 255, 255)
+        cv.circle(frame_copy, node, dot_size, white, circle_type)
+    for subgraph in markers['2']:
+        red = (0, 0, 255)
+        cv.circle(frame_copy, subgraph['c'], dot_size, red, circle_type)
+    for subgraph in markers['3']:
+        green = (0, 255, 0)
+        cv.circle(frame_copy, subgraph['c'], dot_size, green, circle_type)
+    for subgraph in markers['4']:
+        blue = (255, 0, 0)
+        cv.circle(frame_copy, subgraph['c'], dot_size, blue, circle_type)
+
 
 while cap.isOpened():
     # Capture frame-by-frame
@@ -31,6 +49,8 @@ while cap.isOpened():
         print("Can't receive frame (stream end?). Exiting ...")
         break
 
+    frame_copy = frame.copy()
+
     # Display the resulting frame
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     ret, thresh = cv.threshold(gray, 110, 255, cv.THRESH_BINARY)
@@ -38,7 +58,6 @@ while cap.isOpened():
     contours, hierarchy = cv.findContours(thresh, cv.RETR_CCOMP, cv.CHAIN_APPROX_NONE)
     # with_contours = cv.drawContours(frame, contours, -1, (0, 255, 0), 1)
 
-    markers = []
     hashMap = hash.HashMap(width_section, height_section)
 
     # Draw 10 * 10 Section
@@ -48,7 +67,7 @@ while cap.isOpened():
 
     for i in contours:
         M = cv.moments(i)
-        if M['m00'] != 0:
+        if M['m00'] != 0 and cv.contourArea(i) < 100:
             cX = int(M['m10'] / M['m00'])
             cY = int(M['m01'] / M['m00'])
 
@@ -57,9 +76,10 @@ while cap.isOpened():
             # cv.drawContours(frame, [i], 0, (0, 0, 255), 1)
 
     # Visualize hashed points
+
+
     visual_grid = False
     if visual_grid:
-        frame_copy = frame.copy()
         for sec in hashMap.grid:
             for point in hashMap.getPointsFromKey(sec):
                 color = (0, int(800 * (sec[0] % 2) / section_num), int(800 * (sec[1] % 2) / section_num))
@@ -68,17 +88,17 @@ while cap.isOpened():
 
     # Find dot cluster section by section
     visual_marker_edge = True
-    markers = {}
+    markers = {'1': [], '2': [], '3': [], '4': []}
     for key, points in hashMap.grid.items():
         edges = []
         nodes = []
         n = len(points)
         for i in range(n-1):
             for j in range(i+1, n):
-                nodes.append(points[i])
-                nodes.append(points[j])
                 dst = (points[i][0] - points[j][0])**2 + (points[i][1] - points[j][1])**2
-                if dst < 200:
+                if dst < 300:
+                    nodes.append(points[i])
+                    nodes.append(points[j])
                     edge = [points[i], points[j]]
                     if visual_marker_edge:
                         cv.line(frame, edge[0], edge[1], (255, 0, 0), 2, 1)
@@ -86,23 +106,25 @@ while cap.isOpened():
         # Identify markers
         node_sets = set(nodes)      # Remove duplicates
         nodes = list(node_sets)
-        marker_1 = list(set(points) - node_sets)        # Saving single points which are not constructing edges
-        subtrees = marker.findSubtreesInBFS(nodes, edges)
+        markers['1'] = list(set(points) - node_sets)        # Saving single points which are not constructing edges
+        subtrees = marker.findSubgraphsInBFS(nodes, edges)
         for i, subtree in enumerate(subtrees):
-            if len(subtree[i]['n']) == 2:
-                marker_2 = subtree[i]
-            elif len(subtree[i]['n']) == 3:
-                marker_3 = subtree[i]
-            elif len(subtree[i]['n']) == 4:
-                marker_4 = subtree[i]
+            if len(subtree['n']) == 2:
+                markers['2'].append(subtree)
+            elif len(subtree['n']) == 3:
+                markers['3'].append(subtree)
+            elif len(subtree['n']) == 4:
+                markers['4'].append(subtree)
             else:
                 continue
 
+        visualize_marker(markers)
+
     # Show keypoints
     if save_video:
-        out.write(frame)
+        out.write(frame_copy)
     if visual_video:
-        cv.imshow("frame", frame)
+        cv.imshow("frame", frame_copy)
 
     if cv.waitKey(1) == ord('q'):
         break
